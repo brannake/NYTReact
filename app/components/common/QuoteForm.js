cef584d4cbda5332cd07fa486f947e85b89380a6
Kevin Brannan <brannake@gmail.com>
2017-09-04 20:09:39 -0700

    Huehuehue

--- app/components/common/QuoteForm.js
+++ app/components/common/QuoteForm.js
@@ -15,6 +15,7 @@ class QuoteForm extends Component {
     this.handleStartYearInputChange = this.handleStartYearInputChange.bind(this);
     this.handleEndYearInputChange = this.handleEndYearInputChange.bind(this);
     this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.NYTAPICall = this.NYTAPICall.bind(this);
   }
   handleTopicInputChange(event) {
     this.setState({ TopicinputValue: event.target.value });
@@ -30,38 +31,98 @@ class QuoteForm extends Component {
     let topic = searchObject.TopicinputValue;
     let startYear = searchObject.StartYearinputValue;
     let endYear = searchObject.EndYearinputValue;
+    let authKey = "aa7ca2b60183476e8cfcf2c932d2936d";
+    let queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=";
+    let queryURL = queryURLBase + topic;
+      
+    if (parseInt(startYear)) {
+            queryURL = queryURL + "&begin_date=" + startYear + "0101";
+          }
 
-    var authKey = "aa7ca2b60183476e8cfcf2c932d2936d";
-    // Based on the queryTerm we will create a queryURL 
-    var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=";
-    
-    // Array to hold the various article info
-    var articleCounter = 0;
-
-      // The AJAX function uses the URL and Gets the JSON data associated with it. The data then gets stored in the variable called: "NYTData"
-      $.ajax({url: queryURLBase, method: "GET"}) 
-        .done(function(NYTData) {
-    
+    if (parseInt(endYear)) {
+            queryURL = queryURL + "&end_date=" + endYear + "0101";
+          }
+      
+    $.ajax({url: queryURL, method: "GET"}) 
+        .done((NYTData) => {
           // Here we are logging the URL so we have access to it for troubleshooting
-          console.log("------------------------------------")
-          console.log("URL: " + queryURLBase);
-          console.log("------------------------------------")
-    
+          console.log("URL: " + queryURL);
           // Here we then log the NYTData to console, where it will show up as an object.
           console.log(NYTData);
-          console.log("------------------------------------")
-        }
-        )}
+          this.setState({ArticleResponse: NYTData});
+        });
+      };
 
   handleButtonClick() {
-    const searchArticle = this.state;
-    this.NYTAPICall(searchArticle);
+    this.NYTAPICall(this.state);
     this.setState({ TopicinputValue: "",
                     StartYearinputValue: "",
                     EndYearinputValue: "",
                   });
   }
   render() {
+    if (this.state.ArticleResponse) {
+      return (
+        <div className="col-md-6 col-md-offset-3">
+          <div style={styles.formStyle} className="form-group">
+            <label htmlFor="input-box">
+              Add a quote
+            </label>
+            <textarea
+              style={{
+                resize: "none"
+              }}
+              onChange={this.handleTopicInputChange}
+              value={this.state.TopicinputValue}
+              placeholder="Topic"
+              className="form-control"
+              id="input-box"
+              rows="3"
+            />
+            <textarea
+              style={{
+                resize: "none"
+              }}
+              onChange={this.handleStartYearInputChange}
+              value={this.state.StartYearinputValue}
+              placeholder="Start Year"
+              className="form-control"
+              id="input-box"
+              rows="3"
+            />
+            <textarea
+              style={{
+                resize: "none"
+              }}
+              onChange={this.handleEndYearInputChange}
+              value={this.state.EndYearinputValue}
+              placeholder="End Year"
+              className="form-control"
+              id="input-box"
+              rows="3"
+            />
+            <button
+              onClick={this.handleButtonClick}
+              className="btn btn-success"
+              style={styles.buttonStyle}
+            >
+              Submit
+            </button>
+            <textarea
+              style={{
+                resize: "none"
+              }}
+              onChange={this.handleEndYearInputChange}
+              value={this.state.EndYearinputValue}
+              placeholder="End Year"
+              className="form-control"
+              id="input-box"
+              rows="3"
+            />
+          </div>
+        </div>
+      );
+    } else {
     return (
       <div className="col-md-6 col-md-offset-3">
         <div style={styles.formStyle} className="form-group">
@@ -112,6 +173,7 @@ class QuoteForm extends Component {
       </div>
     );
   }
+}
 }
 
 const styles = {
